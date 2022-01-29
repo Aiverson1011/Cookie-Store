58360d4e3170a23f96d62e0a41d2f176c4652825
Amanda Iverson <ajohnson764@gmail.com>
2022-01-29 14:40:23 -0800

    intro to JS

---
+++ app.js
@@ -0,0 +1,17 @@
+'use strict'
+
+let name = prompt('What is your name?');
+let time = prompt('What time is it?');
+let message;
+
+if(time <= 11){
+    message = 'Good Morning!';
+} else if (time <= 18){
+    message = 'Good Afternoon!';
+}  else if(time <= 24){
+    message = "Good Evening!";
+} else {
+    message = "Time does not exist!";
+}
+
+document.write('Hello ' + name + '! ' + message);
