f843f85d3b5fc35a3b2b69bde3dc110c1c3c2e3f
Amanda Iverson <ajohnson764@gmail.com>
2022-01-31 19:36:56 -0800

    update js file for demo and console logs

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 'use strict'
 
+// define the variable named "name" and set the value to the user input of the prompt
 let name = prompt('What is your name?');
-let time = prompt('What time is it?');
-let message;
+console.log('Hello, ' + name);
+
+let time = prompt('What time is it?'); // 15;
+
+// new variable named message equal to an empty string
+let message = '';
+
+/*
+This is a conditional statement determining the value
+of message dependant upon the user input of time from
+the prompt
+*/
 
 if(time <= 11){
     message = 'Good Morning!';
 } else if (time <= 18){
     message = 'Good Afternoon!';
+  
 }  else if(time <= 24){
     message = "Good Evening!";
 } else {
     message = "Time does not exist!";
 }
 
+console.log('Amanda was here');
 document.write('Hello ' + name + '! ' + message);
